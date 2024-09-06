1b4ffee44ee0cd3effc551d0558fffa22385ec98
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-09-06 13:19:05 +0500

    constructor & tokenURI()

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,5 +1,17 @@
 // SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 pragma solidity ^0.8.18;
 
-contract BasicNft {}
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
+}
